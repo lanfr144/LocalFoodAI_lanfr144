776d6a6153665e058db7f27f881e087822445eb7
lanfr144 <lanfr144@student.local>
2026-04-29 12:44:49 +0200

    Add proper reset

---
+++ proper_reset.sh
@@ -0,0 +1,1 @@
+mysql -e "SET GLOBAL log_bin_trust_function_creators = 1; DROP DATABASE IF EXISTS zabbix; CREATE DATABASE zabbix character set utf8mb4 collate utf8mb4_bin; GRANT ALL PRIVILEGES ON zabbix.* TO 'zabbix'@'%'; FLUSH PRIVILEGES;"

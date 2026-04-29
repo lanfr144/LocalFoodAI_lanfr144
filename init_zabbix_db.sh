06df1fda4e5abf3a70360c72c404082980b9af03
lanfr144 <lanfr144@student.local>
2026-04-29 11:40:08 +0200

    Add Zabbix DB init script

---
+++ init_zabbix_db.sh
@@ -0,0 +1,1 @@
+mysql -e "CREATE DATABASE IF NOT EXISTS zabbix character set utf8mb4 collate utf8mb4_bin; CREATE USER IF NOT EXISTS 'zabbix'@'%' IDENTIFIED BY 'zabbix_pwd'; GRANT ALL PRIVILEGES ON zabbix.* TO 'zabbix'@'%'; SET GLOBAL log_bin_trust_function_creators = 1; FLUSH PRIVILEGES;"
